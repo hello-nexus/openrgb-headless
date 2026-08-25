17713bf193580fcaaa5443b71e3beca387c23781
Soldier Alexander <rattorg23@pm.me>
2026-08-25 01:39:40 +0000

    Fix MSI 761 direct mode bounds check comparing a byte offset against an LED count
    (cherry picked from commit 77a474b9933cf10df289fdccfb5050b4556d67b0)

--- Controllers/MSIMysticLightController/MSIMysticLight761Controller/MSIMysticLight761Controller.cpp
+++ Controllers/MSIMysticLightController/MSIMysticLight761Controller/MSIMysticLight761Controller.cpp
@@ -480,7 +480,7 @@ void MSIMysticLight761Controller::SetLedColor
 
     std::size_t candidate_index = (index * 3);
 
-    if((candidate_index + 2) <= GetMaxDirectLeds(zone))
+    if(index < GetMaxDirectLeds(zone))
     {
         set_data_color(ptr, candidate_index,     red);
         set_data_color(ptr, candidate_index + 1, grn);
